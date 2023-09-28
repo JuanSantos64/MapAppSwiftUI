abcc21292c5892563c77ea315cf226fa8c69bcd3
Juan Santos <107580945+JuanSantos64@users.noreply.github.com>
2023-09-28 15:38:46 -0300

    Add files via upload

---
+++ Desafio Mapa/Desafio Mapa/Desafio_MapaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Desafio_MapaApp.swift
+//  Desafio Mapa
+//
+//  Created by Student11 on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Desafio_MapaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
